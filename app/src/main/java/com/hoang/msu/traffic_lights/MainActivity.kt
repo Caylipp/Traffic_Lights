a979de08561ef69cd8ac0ae769a3d543cbdd40a2
Caylipp <philipphoang3005@gmail.com>
2024-01-30 16:26:07 -0600

    Initial Commit: - added colors - added stings - added setbackgroundcolors

--- app/src/main/java/com/hoang/msu/traffic_lights/MainActivity.kt
+++ app/src/main/java/com/hoang/msu/traffic_lights/MainActivity.kt
@@ -3,6 +3,7 @@ package com.hoang.msu.traffic_lights
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.hoang.msu.traffic_lights.databinding.ActivityMainBinding
 
 private lateinit var binding: ActivityMainBinding
@@ -19,18 +20,22 @@ class MainActivity : AppCompatActivity() {
             if(isStop){
                 binding.redImage.visibility = View.VISIBLE
                 binding.yellowImage.visibility = View.INVISIBLE
+                binding.button.setText(R.string.red)
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
                 isStop = false
                 isGo = true
 
             } else if (isGo){
                 binding.redImage.visibility = View.INVISIBLE
                 binding.greenImage.visibility = View.VISIBLE
-                binding.button.setText("GO!")
-
+                binding.button.setText(R.string.green)
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
                 isGo = false
             } else {
                 binding.yellowImage.visibility = View.VISIBLE
                 binding.greenImage.visibility = View.INVISIBLE
+                binding.button.setText(R.string.yellow)
+                binding.button.setBackgroundColor(ContextCompat.getColor(this, R.color.yellow))
                 isStop = true
             }
 
